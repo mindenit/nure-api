9d929cd260c37a131c4edf90dde03ec2ef2901f2
Artem Dadashjants <ketronix2@gmail.com>
2023-11-26 12:49:41 +0200

    fix: fix context

--- Contexts/Context.cs
+++ Contexts/Context.cs
@@ -8,17 +8,24 @@ public class Context : DbContext
     public DbSet<Group> Groups { get; set; }
     public DbSet<Teacher> Teachers { get; set; }
     public DbSet<Auditory> Auditories { get; set; }
-    /*public Context() => Database.EnsureCreatedAsync();*/
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseNpgsql(
             File.ReadAllText("dbConnection"));
     }
-    
-    /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Group>().Property(m => m.Schedule).Is;
+        // Додайте логіку для перевірки наявності таблиць перед викликом EnsureCreatedAsync()
+        if (!modelBuilder.Model.GetEntityTypes().Any())
+        {
+            Database.EnsureCreatedAsync();
+        }
+
+        // Додайте інші налаштування моделі
 
         base.OnModelCreating(modelBuilder);
-    }*/
+    }
 }
+
